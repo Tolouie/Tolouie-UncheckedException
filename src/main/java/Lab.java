97919e3e14aaad3ca44c3d2c4c498ce2110fc3c3
Tou Cheng <louiechenner@gmail.com>
2023-02-22 21:01:53 +0000

    Lab Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,7 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int arr[] = new int[5];
+        arr[5] = 123;
     }
 }
